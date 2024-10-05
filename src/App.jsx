85fc4dbdfa61bb6895f6f54f9f894b50b376cf4a
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2024-10-05 19:11:13 -0300

    Project setup
    Setup of a electron project using react and tailwind.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen max-h-full bg-slate-50">
+      <div className="font-bold text-red-600">Hello World</div>
+    </div>
+  );
+}
