fc3308cff7864a36d5f41c12c15f82640eba24ab
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2024-10-07 16:36:59 -0300

    Theme config save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./views/Home.jsx";
 import Tabs from "./components/Tabs.jsx";
 export default function App() {
-  const [theme, setTheme] = React.useState("light");
+  const storagedTheme = localStorage.getItem('theme')
+  if(!storagedTheme) localStorage.setItem('theme', 'light')
+  const [theme, setTheme] = React.useState(storagedTheme??"light");
   window.electronAPI.onUpdateTheme(
-    (event, newTheme) => !console.log(theme)&&setTheme(newTheme)
+    (event, newTheme) => setTheme(newTheme)
   )
+  useEffect(
+    ()=>{
+      localStorage.setItem('theme', theme)
+    }
+  ,[theme])
   return (
     <div className={"min-h-screen max-h-full bg-slate-50 dark:bg-slate-900 text-slate-900 font-myfont"+' '+(theme==='light'?'':'dark')}>
       <Tabs/>
